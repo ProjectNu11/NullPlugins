1fcdb80404e9956b819ad66e60920f30da9a09dc
nullqwertyuiop <49110142+nullqwertyuiop@users.noreply.github.com>
2022-05-13 23:29:58 +0800

    [Update]

---
+++ search_shortcut/__init__.py
@@ -0,0 +1,46 @@
+import re
+import urllib.parse
+
+from graia.ariadne.app import Ariadne
+from graia.ariadne.event.message import GroupMessage, FriendMessage, MessageEvent
+from graia.ariadne.message.chain import MessageChain
+from graia.ariadne.message.element import Source
+from graia.ariadne.message.parser.twilight import (
+    Twilight,
+    FullMatch,
+    SpacePolicy,
+    WildcardMatch,
+    MatchResult,
+)
+from graia.saya import Saya, Channel
+from graia.saya.builtins.broadcast.schema import ListenerSchema
+
+saya = Saya.current()
+channel = Channel.current()
+
+channel.name("HelpYouSearch")
+channel.author("nullqwertyuiop")
+channel.description("自己查")
+
+
+@channel.use(
+    ListenerSchema(
+        listening_events=[GroupMessage, FriendMessage],
+        inline_dispatchers=[
+            Twilight(
+                [
+                    FullMatch("百度").space(SpacePolicy.FORCE).flags(re.S),
+                    WildcardMatch() @ "content",
+                ]
+            )
+        ],
+    )
+)
+async def search_shortcut(app: Ariadne, event: MessageEvent, content: MatchResult):
+    content = content.result.asDisplay()
+    if content:
+        await app.sendMessage(
+            event.sender.group if isinstance(event, GroupMessage) else event.sender,
+            MessageChain(f"https://www.baidu.com/s?wd={urllib.parse.quote(content)}"),
+            quote=event.messageChain.getFirst(Source),
+        )
