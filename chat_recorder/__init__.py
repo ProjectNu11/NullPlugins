eea1c1e567fee5ce86c41c079262a4bb45d176c9
nullqwertyuiop <49110142+nullqwertyuiop@users.noreply.github.com>
2022-05-17 20:48:34 +0800

    [Update] Add chat recorder

---
+++ chat_recorder/__init__.py
@@ -0,0 +1,77 @@
+import datetime
+import re
+
+import jieba
+from graia.ariadne.event.message import (
+    GroupMessage,
+    MessageEvent,
+    FriendMessage,
+    TempMessage,
+    ActiveGroupMessage,
+    ActiveFriendMessage,
+    ActiveMessage,
+)
+from graia.ariadne.message.element import Plain
+from graia.ariadne.model import Group, Friend
+from graia.saya import Saya, Channel
+from graia.saya.builtins.broadcast.schema import ListenerSchema
+
+from library.orm import orm
+from .table import ChatRecord, SendRecord
+
+saya = Saya.current()
+channel = Channel.current()
+
+channel.name("ChatRecorder")
+channel.author("SAGIRI-kawaii")
+channel.description("一个记录聊天记录的插件，可配合词云等插件使用")
+
+
+@channel.use(
+    ListenerSchema(listening_events=[GroupMessage, FriendMessage, TempMessage])
+)
+async def chat_record(event: MessageEvent):
+    if isinstance(event, GroupMessage):
+        group = event.sender.group.id
+    elif isinstance(event, TempMessage):
+        group = -1
+    else:
+        group = 0
+    message = event.messageChain
+    content = "".join([plain.text for plain in message.get(Plain)]).strip()
+    filter_words = re.findall(r"\[mirai:(.*?)]", content, re.S)
+    for i in filter_words:
+        content = content.replace(f"[mirai:{i}]", "")
+    if content:
+        seg_result = jieba.lcut(content) if content else ""
+        await orm.add(
+            ChatRecord,
+            {
+                "time": datetime.datetime.now(),
+                "group_id": group,
+                "member_id": event.sender.id,
+                "persistent_string": message.asPersistentString()[:4000],
+                "seg": "|".join(seg_result)[:4000] if seg_result else "",
+            },
+        )
+
+
+@channel.use(ListenerSchema(listening_events=[ActiveGroupMessage, ActiveFriendMessage]))
+async def sent_recorder(event: ActiveMessage):
+    supplicant_type = (
+        "group"
+        if isinstance(event.subject, Group)
+        else "friend"
+        if isinstance(event.subject, Friend)
+        else "unknown"
+    )
+    msg = event.messageChain.asPersistentString(binary=False)[:4000]
+    await orm.add(
+        SendRecord,
+        {
+            "time": datetime.datetime.now(),
+            "target": event.subject.id,
+            "type": supplicant_type,
+            "persistent_string": msg,
+        },
+    )
---
+++ chat_recorder/table.py
@@ -0,0 +1,28 @@
+from sqlalchemy import Column, Integer, DateTime, BIGINT, String
+
+from library.orm import Base
+
+
+class ChatRecord(Base):
+    """聊天记录表"""
+
+    __tablename__ = "chat_record"
+
+    id = Column(Integer, primary_key=True)
+    time = Column(DateTime, nullable=False)
+    group_id = Column(BIGINT, nullable=False)
+    member_id = Column(BIGINT, nullable=False)
+    persistent_string = Column(String(length=4000), nullable=False)
+    seg = Column(String(length=4000), nullable=False)
+
+
+class SendRecord(Base):
+    """发送记录表"""
+
+    __tablename__ = "send_record"
+
+    id = Column(Integer, primary_key=True)
+    time = Column(DateTime, nullable=False)
+    target = Column(BIGINT, nullable=False)
+    type = Column(String(length=10), nullable=False)
+    persistent_string = Column(String(length=4000), nullable=False)
