121d0984d3a125647674ec5de8e2ff0d542e34a9
nullqwertyuiop <49110142+nullqwertyuiop@users.noreply.github.com>
2022-06-03 16:59:57 +0800

    [Update] Add dice

---
+++ dice/__init__.py
@@ -0,0 +1,67 @@
+import random
+
+from graia.ariadne import Ariadne
+from graia.ariadne.event.message import (
+    GroupMessage,
+    MessageEvent,
+    FriendMessage,
+)
+from graia.ariadne.message.chain import MessageChain
+from graia.ariadne.message.element import Plain
+from graia.ariadne.message.parser.twilight import (
+    Twilight,
+    RegexMatch,
+    FullMatch,
+    RegexResult,
+)
+from graia.saya import Saya, Channel
+from graia.saya.builtins.broadcast.schema import ListenerSchema
+
+from library.depend import Switch, FunctionCall
+
+saya = Saya.current()
+channel = Channel.current()
+
+channel.name("dice")
+channel.author("nullqwertyuiop")
+channel.description("")
+
+
+@channel.use(
+    ListenerSchema(
+        listening_events=[GroupMessage, FriendMessage],
+        inline_dispatchers=[
+            Twilight(
+                [
+                    RegexMatch(r"\d+") @ "times",
+                    FullMatch("d"),
+                    RegexMatch(r"\d+") @ "faces",
+                ]
+            )
+        ],
+        decorators=[Switch.check(channel.module), FunctionCall.record(channel.module)],
+    )
+)
+async def chat_record(
+    app: Ariadne, event: MessageEvent, times: RegexResult, faces: RegexResult
+):
+    times = int(times.result.asDisplay())
+    if times > 1000:
+        return await app.sendMessage(
+            event.sender.group if isinstance(event, GroupMessage) else event.sender,
+            MessageChain("投掷次数过多"),
+        )
+
+    faces = int(faces.result.asDisplay())
+    await app.sendMessage(
+        event.sender.group if isinstance(event, GroupMessage) else event.sender,
+        MessageChain.create(
+            [
+                Plain(
+                    " ".join(
+                        [f"{random.randint(1, faces)}/{faces}" for _ in range(times)]
+                    )
+                )
+            ]
+        ),
+    )
