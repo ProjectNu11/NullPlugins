ba299daa98671250582428e6dc9b8aefb5314022
nullqwertyuiop <49110142+nullqwertyuiop@users.noreply.github.com>
2022-07-04 21:55:31 +0800

    [Fix]

--- fake_diagnose/__init__.py
+++ fake_diagnose/__init__.py
@@ -7,6 +7,8 @@ from graia.ariadne.message.parser.twilight import RegexMatch, Twilight
 from graia.saya.builtins.broadcast.schema import ListenerSchema
 from graia.saya.channel import Channel
 
+from library.depend import Switch, FunctionCall
+
 channel = Channel.current()
 
 
@@ -14,12 +16,13 @@ channel = Channel.current()
     ListenerSchema(
         listening_events=[GroupMessage],
         inline_dispatchers=[Twilight([RegexMatch(r"\w+(疼|痛)\w+")])],
+        decorators=[Switch.check(channel.module), FunctionCall.record(channel.module)],
     )
 )
 async def fake_diagnose(
     app: Ariadne, event: MessageEvent, source: Source, message: MessageChain
 ):
-    msg = message.as_display
+    msg = message.as_display()
     msg = msg.replace("疼", "癌").replace("痛", "癌")
     cut = list(jieba.cut(msg.split("癌")[0]))
     illness = f"{cut[-1]}癌"
