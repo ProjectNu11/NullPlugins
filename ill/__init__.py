7c4e3e99f390f990a52d1c34d3f8b1821313d292
nullqwertyuiop <49110142+nullqwertyuiop@users.noreply.github.com>
2022-07-09 21:04:52 +0800

    [Feature] Add ill

---
+++ ill/__init__.py
@@ -0,0 +1,61 @@
+import json
+import random
+from pathlib import Path
+
+from graia.ariadne import Ariadne
+from graia.ariadne.event.message import GroupMessage, FriendMessage, MessageEvent
+from graia.ariadne.message.chain import MessageChain
+from graia.ariadne.message.element import At
+from graia.ariadne.message.parser.twilight import (
+    Twilight,
+    FullMatch,
+    SpacePolicy,
+    ElementMatch,
+    ParamMatch,
+    ElementResult,
+    RegexResult,
+)
+from graia.saya import Channel
+from graia.saya.builtins.broadcast import ListenerSchema
+
+from library import config
+from library.depend import Switch, FunctionCall
+
+channel = Channel.current()
+
+assets_path = Path(Path(__file__).parent, "assets")
+with Path(assets_path, "ill_templates.json").open("r", encoding="UTF-8") as f:
+    TEMPLATES = json.loads(f.read())["data"]
+
+
+@channel.use(
+    ListenerSchema(
+        [GroupMessage, FriendMessage],
+        inline_dispatchers=[
+            Twilight(
+                [
+                    FullMatch(config.func.prefix).space(SpacePolicy.NOSPACE),
+                    FullMatch("发病"),
+                    ElementMatch(At, optional=True) @ "at",
+                    ParamMatch(optional=True) @ "text",
+                ]
+            )
+        ],
+        decorators=[Switch.check(channel.module), FunctionCall.record(channel.module)],
+    )
+)
+async def ill(app: Ariadne, event: MessageEvent, at: ElementResult, text: RegexResult):
+    if at.matched:
+        _target = at.result.target
+        if _target_member := await app.get_member(event.sender.group, _target):
+            target = _target_member.name
+        else:
+            target = _target
+    elif text.matched:
+        target = text.result.display
+    else:
+        target = event.sender.name
+    await app.send_message(
+        event.sender.group if isinstance(event, GroupMessage) else event.sender,
+        MessageChain(random.choice(TEMPLATES).format(target=target)),
+    )
