7a544e3b635dc8a3fc5ddde62aa0351193cc9b29
Poiuy Trew Qllun 泊伊尤·特洛·奇恩 <git@nullqwertyuiop.me>
2022-07-29 17:30:17 +0800

    :pencil2: Blackify code

--- random_dick_length/__init__.py
+++ random_dick_length/__init__.py
@@ -36,6 +36,7 @@ from library.depend import Switch, FunctionCall
 
 channel = Channel.current()
 
+
 @channel.use(
     ListenerSchema(
         [GroupMessage, FriendMessage],
@@ -47,27 +48,26 @@ channel = Channel.current()
                 ]
             )
         ],
-        decorators=[Switch.check(channel.module),
-                    FunctionCall.record(channel.module)],
+        decorators=[Switch.check(channel.module), FunctionCall.record(channel.module)],
     )
 )
 async def random_dick_length(app: Ariadne, event: MessageEvent):
     RandomSeed(event.sender)
-    if random.randint(0,1) == 1:
+    if random.randint(0, 1) == 1:
         boki_status = "勃起"
         angle_status = "boki"
     else:
         boki_status = "软掉"
         angle_status = ""
-    angle = str(random.randint(0,180))+"度"
-    if random.randint(0,2) == 0:
+    angle = str(random.randint(0, 180)) + "度"
+    if random.randint(0, 2) == 0:
         phimosis_status = "包茎"
-    elif random.randint(0,2) == 1:
+    elif random.randint(0, 2) == 1:
         phimosis_status = "半包茎"
     else:
         phimosis_status = "非包茎"
-    dick_hardness = "莫氏硬度为" + str(random.randint(1,10))
-    egg_weight = str(random.randint(0,1000)) + "克"
+    dick_hardness = "莫氏硬度为" + str(random.randint(1, 10))
+    egg_weight = str(random.randint(0, 1000)) + "克"
     dick_legth = random.randint(-10, 30)
     if dick_legth > 20:
         dick_length_evaluate = "哪来的兽人，怎么会这么长！"
@@ -83,10 +83,13 @@ async def random_dick_length(app: Ariadne, event: MessageEvent):
         dick_length_evaluate = "dick ... 他 .. 他..他缩进去了！"
     length_text = f"{str(dick_legth)}cm的牛子，{dick_length_evaluate}"
     await app.send_message(
-        event.sender.group if isinstance(
-            event, GroupMessage) else event.sender,
-        MessageChain(f"你今天有一根{boki_status}的,{angle_status}角度为{angle}的{phimosis_status}的{dick_hardness},并且蛋蛋{egg_weight}的{length_text}"),
-     )
+        event.sender.group if isinstance(event, GroupMessage) else event.sender,
+        MessageChain(
+            f"你今天有一根{boki_status}的,{angle_status}角度为{angle}的{phimosis_status}的{dick_hardness},并且蛋蛋{egg_weight}的{length_text}"
+        ),
+    )
     random.seed()
+
+
 def RandomSeed(supplicant: Member | Friend):
     random.seed(int(f"{datetime.now().strftime('%Y%m%d')}{supplicant.id}"))
